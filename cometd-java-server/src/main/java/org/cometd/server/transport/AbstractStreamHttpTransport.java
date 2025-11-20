df36823650001c7887b474a9f72dd6a605b9874b
Dariusz Kaczynski <dariusz.kaczynski@cumulocity.com>
2025-11-20 13:09:19 +0100

    MTM-65247 [SB3.0] Migrate from javax to jakarta - java changes

--- cometd-java-server/src/main/java/org/cometd/server/transport/AbstractStreamHttpTransport.java
+++ cometd-java-server/src/main/java/org/cometd/server/transport/AbstractStreamHttpTransport.java
@@ -19,11 +19,11 @@ import org.cometd.bayeux.server.ServerMessage;
 import org.cometd.server.BayeuxServerImpl;
 import org.cometd.server.ServerSessionImpl;
 
-import javax.servlet.AsyncContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.AsyncContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.ArrayList;
